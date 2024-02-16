849a74df30aeb07779694474fcc74e557a8acd0c
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-02-16 00:28:09 -0800

    Forgot to commit experiment

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -29,39 +29,77 @@ class MyClass {
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(1000, 2000,WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
+    // create an instance of the class  
     myInstance = new MyClass(VALUE1, VALUE2);
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+
 }
+
+let angle = 0;
+let pressed = false;
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(220);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    fill(140,100,0);
+    //rotateX(angle);
+    //rotateY(angle);
+    rotateY(angle);
+    rotateZ(angle);
+    rotateX(angle);
+    if(pressed){
+        angle += 0.005;
+    }
+    rotateX(45);
+    push()
+    torus(200,100);
+    translate(0,0,100);
+    fill(255,192,203)
+    torus(200,75)
+    translate(-150,500,0);
+    rotateX(30);
+    fill(255,0,0);
+    cylinder(30,100);
+    translate(200,40,0);
+    fill(255,255,255);
+    ellipsoid(100,40,20);
+    pop()
+    
+    translate(0,0,-200);
+    
+    push()
+    translate(-280,600,0);
+    rotateX(30);
+    box(50,300,10);
+    pop()
+    
+    push()
+    translate(280,600,0);
+    rotateX(30);
+    box(50,300,10);
+    pop()
+    
+    box(700,900,10);
+
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
+    if(pressed){
+        pressed = false;
+      }
+      else{
+        pressed = true;
+      }
 }
